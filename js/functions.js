2d223f4ff74016c0bc24e4274ff0eae23c25d8e1
Gretata <54635646+Gretata@users.noreply.github.com>
2019-10-04 10:03:13 +0300

    generuoju funkija duomenu eilutems

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,25 @@
 function renderContent( target, data ) {
     let HTML = '';
+    for (let i = 0; i < data.length; i++) {
+        const stuff = data[i];
+
+    HTML += `<div class="table-row">
+        <div class="cell">${stuff.month}</div>
+        // <div class="cell">${months}</div>
+        <div class="cell">${stuff.income}</div>
+        <div class="cell">${stuff.expense}</div>
+        <div class="cell">${balance}</div>
+        </div>`; 
+    }
+return document.querySelector(target).innerHTML = HTML;
+}
+
+
+
+
+
+
+
 
 // return document.getElementById(target).innerHTML = HTML;
 
@@ -13,3 +33,11 @@ for ( let i=0; i<data.length; i++ ) {
                 <div class="cell">${data[i].title}</div>
                 </div>`;
                 
+
+
+                // function renderBlocks( target, data ) {
+                //     let HTML = '';
+                //     let quality = 0;
+                
+                //     for ( let i=0; i<data.length; i++ ) {
+                //         const obj = data[i];
